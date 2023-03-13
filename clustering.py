84bba38dd7ad71c2ccfad15ad9e45c91abc173a3
nadjaq <nadja.bulajic@qcerris.com>
2023-03-13 12:28:08 +0100

    Uradila sam hijerarhijsko klasterovanje i dodala PCA

--- clustering.py
+++ clustering.py
@@ -1,15 +1,19 @@
 ###K-MEANS###
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import isnan, when, count, col
-from pyspark.ml.feature import VectorAssembler, MinMaxScaler
-from pyspark.ml.clustering import KMeans
-from pyspark.ml.evaluation import ClusteringEvaluator
-from pyspark.sql.functions import udf
-from pyspark.sql.types import DoubleType
-from pyspark.ml.feature import VectorAssembler
 import pyspark.sql.functions as F
-import matplotlib.pyplot as plt
+from pyspark.sql.functions import isnan, when, count, col, udf
+from pyspark.sql import SparkSession
+from pyspark.sql.types import DoubleType
 
+from pyspark.ml.feature import VectorAssembler, MinMaxScaler
+from pyspark.ml.clustering import KMeans, BisectingKMeans, BisectingKMeansModel
+from pyspark.ml.evaluation import ClusteringEvaluator
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.feature import PCA as PCAml
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
 
 spark = SparkSession.builder.master("local[*]").appName("Clustering").getOrCreate()
 print("########################")
@@ -52,7 +56,7 @@ columns_names = ["sepal_length_last", "sepal_width_last", "petal_length_last", "
 for column in columns_names:
     pdf = data2.select(F.col(column)).toPandas()
     pdf.plot.box(title="Boxplot of Values for " + column)
-    plt.show()
+    # plt.show()
 
 #sepal_width_last ima autlajere - uklanjanje
 summary_stats = data2.select("sepal_width_last").summary("25%", "75%")
@@ -72,7 +76,7 @@ print(df_no_outliers.count())
 
 pdf = df_no_outliers.select(F.col("sepal_width_last")).toPandas()
 pdf.plot.box(title="Boxplot of Values for " + "sepal_width_last")
-plt.show()
+# plt.show()
 
 # 4. pravljenje modela
     # grupisanje
@@ -84,9 +88,9 @@ print("###Transformisani podaci###")
 data_transformed.show(5)
 
     # izrada modela
+print("###Izrada K-menas modela###")
 kmeans = KMeans(featuresCol="features").setK(3).setSeed(3)
 model = kmeans.fit(data_transformed)  
-print(model)
 print(model.transform(data_transformed).groupBy("prediction").count().show())
 
 # 6. provera modela
@@ -102,4 +106,49 @@ print("Silhouette with squared euclidean distance = " + str(silhouette))
 predictions.show(5)
 
 print(predictions.groupBy("variety", "prediction").count().show())
+
+# Hijerarhijsko klasterovanje
+print("###Izrada modela za hijerarhijsko kalsterovanje###")
+hierarchical_clustering = BisectingKMeans(featuresCol="features").setK(3).setSeed(3)
+model = hierarchical_clustering.fit(data_transformed)
+print(model.transform(data_transformed).groupBy("prediction").count().show())
+predictions2 = model.transform(data_transformed)
+
+# provera
+silhouette = evaluator.evaluate(predictions2)
+print("Silhouette with squared euclidean distance = " + str(silhouette))
+
+predictions2.show(5)
+
+print(predictions2.groupBy("variety", "prediction").count().show())
+
+#vizuelizacija sa PCA
+pca = PCAml(k=2, inputCol="features", outputCol="pca")
+pca_model = pca.fit(data_transformed)
+pca_transformed = pca_model.transform(data_transformed)
+
+
+X_pca = pca_transformed.rdd.map(lambda row: row.pca).collect()
+X_pca = np.array(X_pca)
+
+print("###Ovo je sada za k-means###")
+cluster_assignment = np.array(predictions.rdd.map(lambda row: row.prediction).collect()).reshape(-1,1)
+
+pca_data = np.hstack((X_pca,cluster_assignment))
+
+pca_df = pd.DataFrame(data=pca_data, columns=("1st_principal", "2nd_principal","cluster_assignment"))
+sns.FacetGrid(pca_df,hue="cluster_assignment", height=6).map(plt.scatter, '1st_principal', '2nd_principal' ).add_legend()
+plt.title("K-means")
+plt.show()
+
+
+print("###Ovo je sada hierarhijsko")
+cluster_assignment = np.array(predictions2.rdd.map(lambda row: row.prediction).collect()).reshape(-1,1)
+
+pca_data = np.hstack((X_pca,cluster_assignment))
+
+pca_df = pd.DataFrame(data=pca_data, columns=("1st_principal", "2nd_principal","cluster_assignment"))
+sns.FacetGrid(pca_df,hue="cluster_assignment", height=6).map(plt.scatter, '1st_principal', '2nd_principal' ).add_legend()
+plt.title("Hijerarhijsko klasterovanje###")
+plt.show()
 
